69c8bbe018d8c72d40e90b43b84e23849075b198
Nick Panchy <panchy@sage.nimbios.org>
2018-11-12 16:10:54 -0500

    Modified solution 04

--- solutions/04_wrong-predictions.py
+++ solutions/04_wrong-predictions.py
@@ -1,21 +1,15 @@
 for i in incorrect_idx:
     print('%d: Predicted %d True label %d' % (i, pred_y[i], test_y[i]))
 
-# Plot two dimensions
-
-colors = ["darkblue", "darkgreen", "gray"]
-
-for n, color in enumerate(colors):
+for n in np.unique(test_y):
     idx = np.where(test_y == n)[0]
-    plt.scatter(test_X[idx, 1], test_X[idx, 2],
-                color=color, label="Class %s" % str(n))
+    plt.scatter(test_X[idx, 1], test_X[idx, 2], label="Class %s" % str(iris.target_names[n]))
 
-for i, marker in zip(incorrect_idx, ['x', 's', 'v']):
-    plt.scatter(test_X[i, 1], test_X[i, 2],
+plt.scatter(test_X[incorrect_idx, 1], test_X[incorrect_idx, 2],
                 color="darkred",
-                marker=marker,
+                marker="x",
                 s=40,
-                label=i)
+                label="missclass")
 
 plt.xlabel('sepal width [cm]')
 plt.ylabel('petal length [cm]')
